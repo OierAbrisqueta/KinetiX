819fabcc28fab617417e181f157aaf5686d45361
OierAbrisqueta <oabrisketa76@gmail.com>
2026-03-22 22:31:05 +0100

    Se definen las estructuras

---
+++ include/modelos.h
@@ -0,0 +1,39 @@
+#ifndef KINETIX_MODELOS_H
+#define KINETIX_MODELOS_H
+
+typedef struct {
+    int id_estacion;
+    char nombre[51];
+    char direccion[101];
+    int capacidad_max;
+    int disponibilidad_actual;
+} Estacion;
+
+typedef struct {
+    int id_vehiculo;
+    char tipo;
+    float bateria;
+    int id_estacion;
+    char estado;
+} Vehiculo;
+
+typedef struct {
+    int id_usuario;
+    char dni[10];
+    char nombre[51];
+    float saldo;
+    char contrasena[51];
+} Usuario;
+
+typedef struct {
+    int id_alquiler;
+    int id_usuario;
+    int id_vehiculo;
+    int id_estacion_origen;
+    int id_estacion_destino;
+    char fecha_inicio[20];
+    char fecha_fin[20];
+    float coste_total;
+} Alquiler;
+
+#endif //KINETIX_MODELOS_H
