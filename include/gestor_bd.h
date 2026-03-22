f524a0f363dfaba8eb2c67443b2217520b3410ad
OierAbrisqueta <oabrisketa76@gmail.com>
2026-03-23 00:49:44 +0100

    Actualizo header

--- include/gestor_bd.h
+++ include/gestor_bd.h
@@ -10,5 +10,11 @@ int insertar_estacion(Estacion nueva_estacion);
 int insertar_vehiculo(Vehiculo nuevo_vehiculo);
 int insertar_usuario(Usuario nuevo_usuario);
 int insertar_alquiler(Alquiler nuevo_alquiler);
+int borrar_estacion(int id_estacion);
+int dar_de_baja_vehiculo(int id_vehiculo);
+int dar_de_baja_usuario(int id_usuario);
+int actualizar_estacion(Estacion estacion_actualizada);
+int actualizar_usuario(Usuario usuario_actualizado);
+int actualizar_vehiculo(Vehiculo vehiculo_actualizado);
 
 #endif //KINETIX_GESTOR_BD_H
